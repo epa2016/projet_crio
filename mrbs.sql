6e70d841a98517cacdc482156c0193c9322ac610
epa2016 <dramane.bamba@gmail.com>
2016-04-06 00:23:57 +0200

    Added files via upload

---
+++ mrbs.sql
@@ -0,0 +1,271 @@
+-- phpMyAdmin SQL Dump
+-- version 4.1.14
+-- http://www.phpmyadmin.net
+--
+-- Client :  127.0.0.1
+-- Généré le :  Mar 05 Avril 2016 à 17:10
+-- Version du serveur :  5.6.17
+-- Version de PHP :  5.5.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Base de données :  `mrbs`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `mrbs_area`
+--
+
+CREATE TABLE IF NOT EXISTS `mrbs_area` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `disabled` tinyint(1) NOT NULL DEFAULT '0',
+  `area_name` varchar(30) DEFAULT NULL,
+  `timezone` varchar(50) DEFAULT NULL,
+  `area_admin_email` text,
+  `resolution` int(11) DEFAULT NULL,
+  `default_duration` int(11) DEFAULT NULL,
+  `default_duration_all_day` tinyint(1) NOT NULL DEFAULT '0',
+  `morningstarts` int(11) DEFAULT NULL,
+  `morningstarts_minutes` int(11) DEFAULT NULL,
+  `eveningends` int(11) DEFAULT NULL,
+  `eveningends_minutes` int(11) DEFAULT NULL,
+  `private_enabled` tinyint(1) DEFAULT NULL,
+  `private_default` tinyint(1) DEFAULT NULL,
+  `private_mandatory` tinyint(1) DEFAULT NULL,
+  `private_override` varchar(32) DEFAULT NULL,
+  `min_create_ahead_enabled` tinyint(1) DEFAULT NULL,
+  `min_create_ahead_secs` int(11) DEFAULT NULL,
+  `max_create_ahead_enabled` tinyint(1) DEFAULT NULL,
+  `max_create_ahead_secs` int(11) DEFAULT NULL,
+  `min_delete_ahead_enabled` tinyint(1) DEFAULT NULL,
+  `min_delete_ahead_secs` int(11) DEFAULT NULL,
+  `max_delete_ahead_enabled` tinyint(1) DEFAULT NULL,
+  `max_delete_ahead_secs` int(11) DEFAULT NULL,
+  `max_per_day_enabled` tinyint(1) NOT NULL DEFAULT '0',
+  `max_per_day` int(11) NOT NULL DEFAULT '0',
+  `max_per_week_enabled` tinyint(1) NOT NULL DEFAULT '0',
+  `max_per_week` int(11) NOT NULL DEFAULT '0',
+  `max_per_month_enabled` tinyint(1) NOT NULL DEFAULT '0',
+  `max_per_month` int(11) NOT NULL DEFAULT '0',
+  `max_per_year_enabled` tinyint(1) NOT NULL DEFAULT '0',
+  `max_per_year` int(11) NOT NULL DEFAULT '0',
+  `max_per_future_enabled` tinyint(1) NOT NULL DEFAULT '0',
+  `max_per_future` int(11) NOT NULL DEFAULT '0',
+  `max_duration_enabled` tinyint(1) NOT NULL DEFAULT '0',
+  `max_duration_secs` int(11) NOT NULL DEFAULT '0',
+  `max_duration_periods` int(11) NOT NULL DEFAULT '0',
+  `custom_html` text,
+  `approval_enabled` tinyint(1) DEFAULT NULL,
+  `reminders_enabled` tinyint(1) DEFAULT NULL,
+  `enable_periods` tinyint(1) DEFAULT NULL,
+  `confirmation_enabled` tinyint(1) DEFAULT NULL,
+  `confirmed_default` tinyint(1) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `uq_area_name` (`area_name`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=2 ;
+
+--
+-- Contenu de la table `mrbs_area`
+--
+
+INSERT INTO `mrbs_area` (`id`, `disabled`, `area_name`, `timezone`, `area_admin_email`, `resolution`, `default_duration`, `default_duration_all_day`, `morningstarts`, `morningstarts_minutes`, `eveningends`, `eveningends_minutes`, `private_enabled`, `private_default`, `private_mandatory`, `private_override`, `min_create_ahead_enabled`, `min_create_ahead_secs`, `max_create_ahead_enabled`, `max_create_ahead_secs`, `min_delete_ahead_enabled`, `min_delete_ahead_secs`, `max_delete_ahead_enabled`, `max_delete_ahead_secs`, `max_per_day_enabled`, `max_per_day`, `max_per_week_enabled`, `max_per_week`, `max_per_month_enabled`, `max_per_month`, `max_per_year_enabled`, `max_per_year`, `max_per_future_enabled`, `max_per_future`, `max_duration_enabled`, `max_duration_secs`, `max_duration_periods`, `custom_html`, `approval_enabled`, `reminders_enabled`, `enable_periods`, `confirmation_enabled`, `confirmed_default`) VALUES
+(1, 0, 'D206', 'Europe/Paris', NULL, 3600, 3600, 0, 10, 0, 21, 0, 1, 1, 1, 'none', 1, 0, 1, 1209600, 1, 0, 0, -1800, 1, 2, 1, 10, 1, 30, 0, 50, 0, 100, 1, 7200, 2, NULL, 0, 1, 0, 0, 0);
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `mrbs_entry`
+--
+
+CREATE TABLE IF NOT EXISTS `mrbs_entry` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `start_time` int(11) NOT NULL DEFAULT '0',
+  `end_time` int(11) NOT NULL DEFAULT '0',
+  `entry_type` int(11) NOT NULL DEFAULT '0',
+  `repeat_id` int(11) DEFAULT NULL,
+  `room_id` int(11) NOT NULL DEFAULT '1',
+  `timestamp` timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `create_by` varchar(80) NOT NULL DEFAULT '',
+  `modified_by` varchar(80) NOT NULL DEFAULT '',
+  `name` varchar(80) NOT NULL DEFAULT '',
+  `type` char(1) NOT NULL DEFAULT 'E',
+  `description` text,
+  `status` tinyint(3) unsigned NOT NULL DEFAULT '0',
+  `reminded` int(11) DEFAULT NULL,
+  `info_time` int(11) DEFAULT NULL,
+  `info_user` varchar(80) DEFAULT NULL,
+  `info_text` text,
+  `ical_uid` varchar(255) NOT NULL DEFAULT '',
+  `ical_sequence` smallint(6) NOT NULL DEFAULT '0',
+  `ical_recur_id` varchar(16) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `room_id` (`room_id`),
+  KEY `repeat_id` (`repeat_id`),
+  KEY `idxStartTime` (`start_time`),
+  KEY `idxEndTime` (`end_time`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `mrbs_repeat`
+--
+
+CREATE TABLE IF NOT EXISTS `mrbs_repeat` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `start_time` int(11) NOT NULL DEFAULT '0',
+  `end_time` int(11) NOT NULL DEFAULT '0',
+  `rep_type` int(11) NOT NULL DEFAULT '0',
+  `end_date` int(11) NOT NULL DEFAULT '0',
+  `rep_opt` varchar(32) NOT NULL DEFAULT '',
+  `room_id` int(11) NOT NULL DEFAULT '1',
+  `timestamp` timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `create_by` varchar(80) NOT NULL DEFAULT '',
+  `modified_by` varchar(80) NOT NULL DEFAULT '',
+  `name` varchar(80) NOT NULL DEFAULT '',
+  `type` char(1) NOT NULL DEFAULT 'E',
+  `description` text,
+  `rep_num_weeks` smallint(6) DEFAULT NULL,
+  `month_absolute` smallint(6) DEFAULT NULL,
+  `month_relative` varchar(4) DEFAULT NULL,
+  `status` tinyint(3) unsigned NOT NULL DEFAULT '0',
+  `reminded` int(11) DEFAULT NULL,
+  `info_time` int(11) DEFAULT NULL,
+  `info_user` varchar(80) DEFAULT NULL,
+  `info_text` text,
+  `ical_uid` varchar(255) NOT NULL DEFAULT '',
+  `ical_sequence` smallint(6) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`),
+  KEY `room_id` (`room_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `mrbs_room`
+--
+
+CREATE TABLE IF NOT EXISTS `mrbs_room` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `disabled` tinyint(1) NOT NULL DEFAULT '0',
+  `area_id` int(11) NOT NULL DEFAULT '0',
+  `room_name` varchar(25) NOT NULL DEFAULT '',
+  `sort_key` varchar(25) NOT NULL DEFAULT '',
+  `description` varchar(60) DEFAULT NULL,
+  `capacity` int(11) NOT NULL DEFAULT '0',
+  `room_admin_email` text,
+  `custom_html` text,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `uq_room_name` (`area_id`,`room_name`),
+  KEY `idxSortKey` (`sort_key`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=7 ;
+
+--
+-- Contenu de la table `mrbs_room`
+--
+
+INSERT INTO `mrbs_room` (`id`, `disabled`, `area_id`, `room_name`, `sort_key`, `description`, `capacity`, `room_admin_email`, `custom_html`) VALUES
+(1, 0, 1, 'D206_A', 'D206_A', '', 0, NULL, NULL),
+(2, 0, 1, 'D206_B', 'D206_B', '', 0, NULL, NULL),
+(3, 0, 1, 'D206_C', 'D206_C', '', 0, NULL, NULL),
+(4, 0, 1, 'D206_D', 'D206_D', '', 0, NULL, NULL),
+(5, 0, 1, 'D206_E', 'D206_E', '', 0, NULL, NULL),
+(6, 0, 1, 'D206_F', 'D206_F', '', 0, NULL, NULL);
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `mrbs_users`
+--
+
+CREATE TABLE IF NOT EXISTS `mrbs_users` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `level` smallint(6) NOT NULL DEFAULT '0',
+  `name` varchar(30) DEFAULT NULL,
+  `password_hash` varchar(255) DEFAULT NULL,
+  `email` varchar(75) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `uq_name` (`name`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=2 ;
+
+--
+-- Contenu de la table `mrbs_users`
+--
+
+INSERT INTO `mrbs_users` (`id`, `level`, `name`, `password_hash`, `email`) VALUES
+(1, 2, 'administrateur', '$2y$10$MaymW/ulX8f8cNwni.9G8OiyfA2WHDnhCl4VTk2KcThKc2JShXwwe', '');
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `mrbs_variables`
+--
+
+CREATE TABLE IF NOT EXISTS `mrbs_variables` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `variable_name` varchar(80) DEFAULT NULL,
+  `variable_content` text,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `uq_variable_name` (`variable_name`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=3 ;
+
+--
+-- Contenu de la table `mrbs_variables`
+--
+
+INSERT INTO `mrbs_variables` (`id`, `variable_name`, `variable_content`) VALUES
+(1, 'db_version', '47'),
+(2, 'local_db_version', '1');
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `mrbs_zoneinfo`
+--
+
+CREATE TABLE IF NOT EXISTS `mrbs_zoneinfo` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `timezone` varchar(255) NOT NULL DEFAULT '',
+  `outlook_compatible` tinyint(3) unsigned NOT NULL DEFAULT '0',
+  `vtimezone` text,
+  `last_updated` int(11) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `uq_timezone` (`timezone`,`outlook_compatible`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+
+--
+-- Contraintes pour les tables exportées
+--
+
+--
+-- Contraintes pour la table `mrbs_entry`
+--
+ALTER TABLE `mrbs_entry`
+  ADD CONSTRAINT `mrbs_entry_ibfk_1` FOREIGN KEY (`room_id`) REFERENCES `mrbs_room` (`id`) ON UPDATE CASCADE,
+  ADD CONSTRAINT `mrbs_entry_ibfk_2` FOREIGN KEY (`repeat_id`) REFERENCES `mrbs_repeat` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Contraintes pour la table `mrbs_repeat`
+--
+ALTER TABLE `mrbs_repeat`
+  ADD CONSTRAINT `mrbs_repeat_ibfk_1` FOREIGN KEY (`room_id`) REFERENCES `mrbs_room` (`id`) ON UPDATE CASCADE;
+
+--
+-- Contraintes pour la table `mrbs_room`
+--
+ALTER TABLE `mrbs_room`
+  ADD CONSTRAINT `mrbs_room_ibfk_1` FOREIGN KEY (`area_id`) REFERENCES `mrbs_area` (`id`) ON UPDATE CASCADE;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
